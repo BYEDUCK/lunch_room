694863fea0115f5c0b86bf9717dc59ba2a6cbe6f
Mateusz Bajdak <mateuszbajdak@gmail.com>
2019-11-13 00:26:17 +0100

    exception resolver

---
+++ src/main/kotlin/byeduck/lunchroom/error/ExceptionResolver.kt
@@ -0,0 +1,24 @@
+package byeduck.lunchroom.error
+
+import byeduck.lunchroom.user.exceptions.InvalidCredentialsException
+import byeduck.lunchroom.user.exceptions.UserAlreadyExistsException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+
+@ControllerAdvice
+class ExceptionResolver {
+
+    @ExceptionHandler(value = [InvalidCredentialsException::class])
+    fun handleInvalidCredentials(exception: InvalidCredentialsException, request: WebRequest): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.message)
+    }
+
+    @ExceptionHandler(value = [UserAlreadyExistsException::class])
+    fun handleUserCollision(exception: UserAlreadyExistsException, request: WebRequest): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with given nick already exists.")
+    }
+
+}
--- src/test/kotlin/byeduck/lunchroom/user/UserServiceImplIT.kt
+++ src/test/kotlin/byeduck/lunchroom/user/UserServiceImplIT.kt
@@ -1,11 +1,16 @@
 package byeduck.lunchroom.user
 
+import byeduck.lunchroom.user.exceptions.InvalidCredentialsException
+import byeduck.lunchroom.user.exceptions.UserAlreadyExistsException
 import byeduck.lunchroom.user.services.UserService
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest
 internal class UserServiceImplIT {
@@ -15,6 +20,7 @@ internal class UserServiceImplIT {
 
     @Test
     @DisplayName("After saving user with given password signIn should return saved user with token")
+    @DirtiesContext
     internal fun basicLoginActionTest() {
         val nick = "testNick"
         val password = "testPassword"
@@ -22,5 +28,26 @@ internal class UserServiceImplIT {
         val loggedInUser = userService.signIn(nick, password)
         assertTrue(loggedInUser.user.id?.isNotEmpty()!!)
         assertTrue(loggedInUser.token.isNotEmpty())
+    }
+
+    @Test
+    @DisplayName("Signing up with user that has been already signed up should result in exception")
+    @DirtiesContext
+    internal fun testSignUpWithAlreadySavedNick() {
+        val nick = "testNick"
+        val password = "testPassword"
+        userService.signUp(nick, password)
+        assertThrows<UserAlreadyExistsException> { userService.signUp(nick, password) }
+    }
+
+    @Test
+    @DisplayName("Signing in with wrong password should result in exception")
+    @DirtiesContext
+    internal fun testSignInWIthWrongPassword() {
+        val nick = "testNick"
+        val password = "testPassword"
+        userService.signUp(nick, password)
+        assertThrows<InvalidCredentialsException> { userService.signIn(nick, "wrongPassword") }
+        assertDoesNotThrow { userService.signIn(nick, password) }
     }
 }
