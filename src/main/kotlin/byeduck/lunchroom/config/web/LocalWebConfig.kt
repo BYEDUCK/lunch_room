9e64c14f7a5891c0afcb5d756bb3b9012b79c257
Mateusz Bajdak <mateuszbajdak@gmail.com>
2019-11-21 22:42:28 +0100

    enable cors

---
+++ src/main/kotlin/byeduck/lunchroom/config/web/LocalWebConfig.kt
@@ -0,0 +1,15 @@
+package byeduck.lunchroom.config.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@Profile("local")
+class LocalWebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+        super.addCorsMappings(registry)
+    }
+}
