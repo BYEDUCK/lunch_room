80bb552dd8de169ae1068ddcb0600185908f725a
Mateusz Bajdak <m.bajdak@samsung.com>
2019-11-26 13:07:58 +0100

    removed @EnableWebMvc

--- src/main/kotlin/byeduck/lunchroom/config/web/HerokuWebConfig.kt
+++ src/main/kotlin/byeduck/lunchroom/config/web/HerokuWebConfig.kt
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @Profile("heroku")
-@EnableWebMvc
 class HerokuWebConfig(
         @Value("\${front.url}")
         private val frontUrl: String,
