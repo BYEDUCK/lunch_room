3b23e290ca3768bd648ed534e42c3c9ef712f6c2
byeduck <mateuszbajdak@gmail.com>
2019-12-15 12:12:51 +0100

    simple test

--- src/main/kotlin/byeduck/lunchroom/config/web/WebSocketConfig.kt
+++ src/main/kotlin/byeduck/lunchroom/config/web/WebSocketConfig.kt
@@ -3,6 +3,7 @@ package byeduck.lunchroom.config.web
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
@@ -12,5 +13,11 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/room")
         registry.setApplicationDestinationPrefixes("/app")
         super.configureMessageBroker(registry)
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/propose").setAllowedOrigins("*")
+        registry.addEndpoint("/propose").setAllowedOrigins("*").withSockJS()
+        super.registerStompEndpoints(registry)
     }
 }
