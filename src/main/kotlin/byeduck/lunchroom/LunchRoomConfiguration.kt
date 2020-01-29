383dc846fc4815aee24cf23ce725c5ca3bc4418e
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-29 11:18:44 +0100

    added logback config

--- src/main/kotlin/byeduck/lunchroom/LunchRoomConfiguration.kt
+++ src/main/kotlin/byeduck/lunchroom/LunchRoomConfiguration.kt
@@ -1,23 +1,13 @@
 package byeduck.lunchroom
 
-import byeduck.lunchroom.logging.LoggingInterceptor
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class LunchRoomConfiguration : WebMvcConfigurer {
-    override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(loggingInterceptor())
-        super.addInterceptors(registry)
-    }
-
-    @Bean
-    fun loggingInterceptor(): LoggingInterceptor = LoggingInterceptor()
-
     @Bean
     fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
 }
