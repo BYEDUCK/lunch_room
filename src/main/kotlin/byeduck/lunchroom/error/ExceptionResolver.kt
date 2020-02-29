f581ae9a2b1a835503f97e3b4c9530b96655bd5c
BYEDUCK <mateuszbajdak@gmail.com>
2020-02-29 12:58:35 +0100

    handle HttpMessageNotWritableException

--- src/main/kotlin/byeduck/lunchroom/error/ExceptionResolver.kt
+++ src/main/kotlin/byeduck/lunchroom/error/ExceptionResolver.kt
@@ -8,6 +8,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotWritableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -70,6 +71,14 @@ class ExceptionResolver {
         logger.error("UNEXPECTED EXCEPTION: {}", exception.message)
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorMessage(ErrorCodes.GENERAL, exception.message))
+    }
+
+    @ExceptionHandler(value = [HttpMessageNotWritableException::class])
+    fun handleUnsupportedContentType(exception: Exception, request: WebRequest): ResponseEntity<ErrorMessage> {
+        logger.error("HTTP MSG NOT WRITABLE: {}", exception.message)
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(ErrorMessage(ErrorCodes.GENERAL, exception.message))
     }
 
